0a2e46b1f0fccc127b1ea0ecf6d81b4ae16fa2e7
v4t <39200058+v4t@users.noreply.github.com>
2018-12-03 20:09:33 +0200

    Include JFX

--- src/main/scala/torvi/Main.scala
+++ src/main/scala/torvi/Main.scala
@@ -1,5 +1,29 @@
 package torvi
 
-object Main extends App {
-  println("TODO")
+import java.nio.file.{Files, Paths}
+
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.{Scene}
+import scalafx.scene.paint.Color._
+
+object Main extends JFXApp {
+  def exitWithError(err: String) = {
+    println(err)
+    sys.exit(0)
+  }
+  if (parameters.unnamed.length == 0)
+    exitWithError("Please provide ROM")
+  if (!Files.exists(Paths.get(parameters.unnamed(0))))
+    exitWithError("Given ROM file does not exist")
+
+  val rom = parameters.unnamed(0)
+  stage = new PrimaryStage {
+    title = "C8Emu"
+    width = 650
+    height = 450
+    scene = new Scene {
+      fill = Black
+    }
+  }
 }
