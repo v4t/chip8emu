0220ab5b9242fa8f845620b64faff44eef521246
torvi <torvi>
2018-12-03 22:30:42 +0200

    Create template for chip 8 emulator

--- src/main/scala/torvi/Main.scala
+++ src/main/scala/torvi/Main.scala
@@ -1,5 +1,16 @@
 package torvi
 
+import java.nio.file.{Paths, Files}
+
 object Main extends App {
+  if(args.length != 1) {
+    println("Usage: /path/to/rom.ch8")
+    sys.exit()
+  }
+  if(!Files.exists(Paths.get(args(0)))) {
+    println("Invalid file specified")
+    sys.exit()
+  }
+
   println("TODO")
 }
