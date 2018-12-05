670f3657c343a30d201dd63c07551b38da50fb98
torvi <torvi>
2018-12-05 23:21:31 +0200

    Initialize font set to memory, add some invalid opcode error handling and add timer handling to emulation cycle

--- src/main/scala/torvi/chip8/exceptions.scala
+++ src/main/scala/torvi/chip8/exceptions.scala
@@ -1,5 +1,7 @@
 package torvi.chip8
 
+class InvalidOpCodeException(s:String) extends Exception(s){}
+
 class RomTooLargeException(s:String) extends Exception(s){}
 
 class StackUnderflowException(s:String) extends Exception(s){}
